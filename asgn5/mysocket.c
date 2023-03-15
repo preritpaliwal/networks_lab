ef3c7f8c55a63d307caa79c1651d402b82093d03
Prerit Paliwal <prerit.paliwal@kgpian.iitkgp.ac.in>
2023-03-15 18:18:27 +0530

    merge done

--- asgn5/mysocket.c
+++ asgn5/mysocket.c
@@ -2,8 +2,8 @@
 #include <pthread.h>
 #include <stdlib.h>
 
-struct MyFD *my_socket(int __domain,int __type,int __protocol){
-    struct MyFD *__fd = (struct MyFD *)malloc(sizeof(struct MyFD));
+MyFD *my_socket(int __domain,int __type,int __protocol){
+    MyFD *__fd = (MyFD *)malloc(sizeof(struct MyFD));
     __fd->sock_fd = socket(__domain,__type,__protocol);
     __fd->flag = 0;
     // TODO: initqueues
@@ -19,45 +19,57 @@ struct MyFD *my_socket(int __domain,int __type,int __protocol){
     return __fd;
 }
 
-void read_loop(struct MyFD *__fd){
+void *read_loop(void *args){
+    MyFD *__fd = (MyFD *)args;
     while(1){
         if(__fd->flag == 1){
             pthread_exit(0);
         }
-        pthread_mutex_lock(lock[0]);
-        if()
+        pthread_mutex_lock(&(__fd->recvTableLock));
+        // if()
     }
+    return 0;
 }
 
-void write_loop(){
+void *write_loop(void *args){
+    MyFD *__fd =  (MyFD *)args;
     while(1){
         if(__fd->flag == 1){
             pthread_exit(0);
         }
+
     }
+    return 0;
 }
 
-int my_bind(struct MyFD *__fd,const struct sockaddr *__addr,socklen_t __len){
+int my_bind(MyFD *__fd,const struct sockaddr *__addr,socklen_t __len){
     return bind(__fd->sock_fd,__addr,__len);
 }
-int my_listen(struct MyFD *__fd, int __n){
+int my_listen(MyFD *__fd, int __n){
     return listen(__fd->sock_fd,__n);
 }
-int my_accept(struct MyFD *__fd,struct sockaddr *__addr,socklen_t *__addr_len){
-    pthread_create()
+int my_accept(MyFD *__fd,struct sockaddr *__addr,socklen_t *__addr_len){
+    if (pthread_create(&(__fd->readThread), NULL, read_loop, __fd) != 0) { // create the read thread
+        perror("Error creating readThread thread!");
+        exit(EXIT_FAILURE);
+    }
+    if (pthread_create(&(__fd->writeThread), NULL, write_loop, __fd) != 0) { // create the write thread
+        perror("Error creating writeThread thread!");
+        exit(EXIT_FAILURE);
+    }
     return accept(__fd->sock_fd,__addr,__addr_len);
 }
-int my_connect(struct MyFD *__fd,struct sockaddr * __addr, socklen_t  __addr_len){
+int my_connect(MyFD *__fd,struct sockaddr * __addr, socklen_t  __addr_len){
     return connect(__fd->sock_fd,__addr,__addr_len);
 }
-ssize_t my_send(struct MyFD *__fd,const void *__buf, size_t __n, int __flags){
+ssize_t my_send(MyFD *__fd,const void *__buf, size_t __n, int __flags){
     // push in table
 }
-ssize_t my_recv(struct MyFD *__fd, void *__buf, size_t __n, int __flags){
+ssize_t my_recv(MyFD *__fd, void *__buf, size_t __n, int __flags){
     // pop from table
 }
 
-int my_close(struct MyFD *__fd){
+int my_close(MyFD *__fd){
     __fd->flag = 1;
     // sleep(5);
     return close(__fd->sock_fd);
--- asgn5/mysocket.h
+++ asgn5/mysocket.h
@@ -1,19 +1,24 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <pthread.h>
 #include "queue.h"
 
 typedef struct MyFD{
     int sock_fd;
     int flag;
-    Q receiveTable;
+    Q recvTable;
     Q sendTable;
-};
+    pthread_t readThread;
+    pthread_t writeThread;
+    pthread_mutex_t recvTableLock;
+    pthread_mutex_t sendTableLock;
+}MyFD;
 
-struct MyFD *my_socket(int __domain,int __type,int __protocol);
-int my_bind(struct MyFD *__fd,const struct sockaddr *__addr,socklen_t __len);
-int my_listen(struct MyFD *__fd, int __n);
-int my_accept(struct MyFD *__fd,struct sockaddr *__addr,socklen_t *__addr_len);
-int my_connect(struct MyFD *__fd,struct sockaddr * __addr, socklen_t  __addr_len);
-ssize_t my_send(struct MyFD *__fd,const void *__buf, size_t __n, int __flags);
-ssize_t my_recv(struct MyFD *__fd, void *__buf, size_t __n, int __flags);
-int my_close(struct MyFD *__fd);
+MyFD *my_socket(int __domain,int __type,int __protocol);
+int my_bind(MyFD *__fd,const struct sockaddr *__addr,socklen_t __len);
+int my_listen(MyFD *__fd, int __n);
+int my_accept(MyFD *__fd,struct sockaddr *__addr,socklen_t *__addr_len);
+int my_connect(MyFD *__fd,struct sockaddr * __addr, socklen_t  __addr_len);
+ssize_t my_send(MyFD *__fd,const void *__buf, size_t __n, int __flags);
+ssize_t my_recv(MyFD *__fd, void *__buf, size_t __n, int __flags);
+int my_close(MyFD *__fd);
